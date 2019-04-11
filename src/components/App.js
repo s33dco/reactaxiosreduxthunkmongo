4714787cc699b5ca14aa8372bf0265607e866538
s33dco <code@s33d.co>
2019-04-11 12:12:22 +0100

    back end and front end up

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import NewPost from './components/NewPost';
-
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import NewPost from './NewPost';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
